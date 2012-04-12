a463e9d31f19adf58eba60291142d522ccebe4d0
Alessandro Berardi <berardialessandro@gmail.com>
2012-04-12 17:02:55 +0930

    unused local variable

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Enumerable do
   
   it "should allow mapping elements of the collection to hashes associating method names to the returned values for each method" do
-    a = [1,2,3].map_methods(:to_s,:to_f).should eql [{:to_s => "1", :to_f => 1.0}, {:to_s => "2", :to_f => 2.0}, {:to_s => "3", :to_f => 3.0}]
+    [1,2,3].map_methods(:to_s,:to_f).should eql [{:to_s => "1", :to_f => 1.0}, {:to_s => "2", :to_f => 2.0}, {:to_s => "3", :to_f => 3.0}]
   end
   
 end
