fb1544a66cede553bd9017524aef31ad2384adf7
Michael Noack <michael@noack.com.au>
2012-07-06 15:14:27 +0930

    Make spec ruby 1.8 compatible

--- spec/compact_spec.rb
+++ spec/compact_spec.rb
@@ -6,11 +6,11 @@ describe Array, 'compact' do
   end
 
   it 'should recursive compact blank' do
-    a = [1, nil, {a: nil, b: 2}]
+    a = [1, nil, {:a => nil, :b => 2}]
     a.recursive_compact_blank!
-    a.should == [1, {b: 2}]
+    a.should == [1, {:b => 2}]
 
-    b = [1, nil, {a: nil}]
+    b = [1, nil, {:a => nil}]
     b.recursive_compact_blank!
     b.should == [1]
   end
@@ -18,32 +18,32 @@ end
 
 describe Hash, 'compact' do
   it 'should compact' do
-    {a: 1, b: nil}.compact.should == {a: 1}
+    {:a => 1, :b => nil}.compact.should == {:a => 1}
   end
 
   it 'should compact!' do
-    a = {a: 1, b: nil}
+    a = {:a => 1, :b => nil}
     a.compact!
-    a.should == {a: 1}
+    a.should == {:a => 1}
   end
 
   it 'should compact blank' do
-    {a: 1, b: ''}.compact_blank.should == {a: 1}
+    {:a => 1, :b => ''}.compact_blank.should == {:a => 1}
   end
 
   it 'should compact blank!' do
-    a = {a: 1, b: ''}
+    a = {:a => 1, :b => ''}
     a.compact_blank!
-    a.should == {a: 1}
+    a.should == {:a => 1}
   end
 
   it 'should recursive compact blank!' do
-    a = {a: 1, b: {c: 1, d: ''}}
+    a = {:a => 1, :b => {:c => 1, :d => ''}}
     a.recursive_compact_blank!
-    a.should == {a: 1, b: {c: 1}}
+    a.should == {:a => 1, :b => {:c => 1}}
 
-    a = {a: 1, b: {c: [], d: ''}}
+    a = {:a => 1, :b => {:c => [], :d => ''}}
     a.recursive_compact_blank!
-    a.should == {a: 1}
+    a.should == {:a => 1}
   end
 end
