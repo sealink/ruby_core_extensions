993f06af0573b5919cc6671c32e771b282c297da
Michael Noack <michael@noack.com.au>
2013-06-03 10:05:55 +0930

    Remove rarely used method * Best defined where actually used * Method was also active record specific so if anything should be in rails_core_extensions

--- lib/ruby_core_extensions/array.rb
+++ lib/ruby_core_extensions/array.rb
@@ -8,18 +8,6 @@ class Array
     first.titleize
   end  
 
-  def id_to_model(klass)
-    if self.blank?
-      self
-    else
-      klass.find_all_by_id(self)
-    end
-  end
-  
-  #def to_hash
-  #  inject({}) { |m, e| m[e[0]] = e[1]; m }
-  #end
-
   # Key should be unique, or latest element with that key will override previous ones.
   def hash_by(key = nil, method = nil, &block)
     self.inject({}) do |h, element|
